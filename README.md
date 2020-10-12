ce1bd5580f91650b463cd5fcb358e678596e35eb
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2020-10-12 15:02:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdivinAPP
+ Ejercicio AdivinAPP
---
+++ src/main/java/dad/maeven/adivinapp/Main.java
@@ -0,0 +1,11 @@
+package dad.maeven.adivinapp;
+
+public class Main {
+
+	/* Método encargado de iniciar la aplicación. */
+	public static void main(String[] args) {
+		AdivinApp.main(args);
+
+	}
+
+}
